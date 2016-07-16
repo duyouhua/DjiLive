786ac6fda2a7d837d8156fa226c3df1bfa51099b
Daniel Chernenkov <me@danielckv.com>
2016-07-16 22:56:34 +0300

    Icons and gradle build

--- app/src/main/java/danielckv/com/djilive/MainActivity.java
+++ app/src/main/java/danielckv/com/djilive/MainActivity.java
@@ -14,8 +14,8 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
 
@@ -27,47 +27,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
-                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
-                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
-                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
-                            Manifest.permission.READ_PHONE_STATE,
-                    }
-                    , 1);
-        }
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setPublishPermissions(Arrays.asList("publish_actions"));
 
         callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
 
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                    }
+            @Override
+            public void onCancel() {
+                Log.e("DJILive::MainAcitivty", "User canceled Facebook login");
+                Toast.makeText(getApplicationContext(), "You have declined the Facebook Dialog access.", Toast.LENGTH_LONG);
+            }
 
-                    @Override
-                    public void onCancel() {
-                        Log.e("DJILive::MainAcitivty", "User canceled Facebook login");
-                        Toast.makeText(getApplicationContext(), "You have declined the Facebook Dialog access.", Toast.LENGTH_LONG);
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Log.e("DJILive::MainAcitivty", exception.toString());
-                        Toast.makeText(getApplicationContext(), "Something went wrong and the Facebook access token have been declined.", Toast.LENGTH_LONG);
-                    }
-                });
-
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile,publish_actions"));
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e("DJILive::MainAcitivty", exception.toString());
+                Toast.makeText(getApplicationContext(), "Something went wrong and the Facebook access token have been declined.", Toast.LENGTH_LONG);
+            }
+        });
     }
 
     @Override
