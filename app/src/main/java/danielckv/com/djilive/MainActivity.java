346fb792e8e3d6f689a2a2cede4fdda34c1c6e3f
Daniel Chernenkov <me@danielckv.com>
2016-07-16 11:49:44 +0300

    Added realm support and Facebook login method

--- app/src/main/java/danielckv/com/djilive/MainActivity.java
+++ app/src/main/java/danielckv/com/djilive/MainActivity.java
@@ -1,17 +1,36 @@
 package danielckv.com.djilive;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ActivityCompat.requestPermissions(this,
@@ -25,6 +44,36 @@ public class MainActivity extends AppCompatActivity {
                     }
                     , 1);
         }
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.e("DJILive::MainAcitivty", "User canceled Facebook login");
+                        Toast.makeText(getApplicationContext(), "You have declined the Facebook Dialog access.", Toast.LENGTH_LONG);
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.e("DJILive::MainAcitivty", exception.toString());
+                        Toast.makeText(getApplicationContext(), "Something went wrong and the Facebook access token have been declined.", Toast.LENGTH_LONG);
+                    }
+                });
+
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile,publish_actions"));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
